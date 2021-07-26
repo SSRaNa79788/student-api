0aed539cf7506402bb3bbed054bf65641432d3cf
SSRaNa79788 <84072393+SSRaNa79788@users.noreply.github.com>
2021-07-26 12:45:48 +0530

    done with basic code structure

--- Controllers/StudentController.go
+++ Controllers/StudentController.go
@@ -1,1 +1,68 @@
+//Controllers/User.go
 package Controllers
+import (
+	"student-api/Models"
+	"fmt"
+	"net/http"
+	"github.com/gin-gonic/gin"
+)
+//GetUsers ... Get all users
+func GetStudents(c *gin.Context) {
+	var student []Models.Student
+	err := Models.GetAllStudents(&student)
+	if err != nil {
+		c.AbortWithStatus(http.StatusNotFound)
+	} else {
+		c.JSON(http.StatusOK, student)
+	}
+}
+//CreateUser ... Create User
+func CreateStudent(c *gin.Context) {
+	var student Models.Student
+	c.BindJSON(&student)
+	err := Models.CreateStudent(&student)
+	if err != nil {
+		fmt.Println(err.Error())
+		c.AbortWithStatus(http.StatusNotFound)
+	} else {
+		c.JSON(http.StatusOK, student)
+	}
+}
+//GetUserByID ... Get the user by id
+func GetStudentByID(c *gin.Context) {
+	id := c.Params.ByName("id")
+	var student Models.Student
+	err := Models.GetStudentByID(&student, id)
+	if err != nil {
+		c.AbortWithStatus(http.StatusNotFound)
+	} else {
+		c.JSON(http.StatusOK, student)
+	}
+}
+//UpdateUser ... Update the user information
+func UpdateStudent(c *gin.Context) {
+	var user Models.Student
+	id := c.Params.ByName("id")
+	err := Models.GetStudentByID(&user, id)
+	if err != nil {
+		c.JSON(http.StatusNotFound, user)
+	}
+	c.BindJSON(&user)
+	err = Models.UpdateStudent(&user, id)
+	if err != nil {
+		c.AbortWithStatus(http.StatusNotFound)
+	} else {
+		c.JSON(http.StatusOK, user)
+	}
+}
+//DeleteUser ... Delete the user
+func DeleteStudent(c *gin.Context) {
+	var student Models.Student
+	id := c.Params.ByName("id")
+	err := Models.DeleteStudent(&student, id)
+	if err != nil {
+		c.AbortWithStatus(http.StatusNotFound)
+	} else {
+		c.JSON(http.StatusOK, gin.H{"id" + id: "is deleted"})
+	}
+}
--- Models/StudentDataModel.go
+++ Models/StudentDataModel.go
@@ -1,1 +1,14 @@
+//Models/UserModel.go
 package Models
+type Student struct {
+	Id      uint   `json:"id"`
+	Name    string `json:"name"`
+	LastName   string `json:"lastname"`
+	DOB   string `json:"dob"`
+	Address string `json:"address"`
+	Subject string `json:"subject"`
+	Mark string `json:"mark"`
+}
+func (b *Student) TableName() string {
+	return "student"
+}
